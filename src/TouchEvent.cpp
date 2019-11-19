09ddd336f50397e64ab53aa00ed67c8db81e28cf
Gerald Lechner <lechge@gmail.com>
2019-11-19 10:32:48 +0100

    Add files via upload

---
+++ src/TouchEvent.cpp
@@ -0,0 +1,166 @@
+//Version 0.1
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+
+#include <Arduino.h>
+#include <XPT2046_Touchscreen.h>
+#include "TouchEvent.h"
+
+//constructor prefill private members
+TouchEvent::TouchEvent(XPT2046_Touchscreen& touch): _touch(touch) {
+}
+void TouchEvent::pollTouchScreen() {
+  TS_Point p;
+  boolean tch;
+  int16_t xr,yr,dx,dy;
+  //first we get position and check if touched
+  p=_touch.getPoint();
+  tch = (p.z > _minPress);
+  if (tch && _down) { //continous touch
+    dx = abs(_last.x-p.x); dy = abs(_last.y-p.y);
+    _last = p;
+    if (_drawMode && ((dx > _moveth) || (dy > _moveth))) { //movement?
+      if (_onTouchDraw) _onTouchDraw(toScreen(p));
+    }
+    if (((millis() - _begin) > _clickLong) && (!_long)) {
+      if (_onTouchLong) _onTouchLong(toScreen(p));
+    }
+  } else if (tch && !_down) { //start of touch
+    _start = p; _last = p; _begin = millis(); _long = false;
+    _down = tch;
+    if (_onTouchDown) _onTouchDown(toScreen(p));
+  } else if (!tch && _down) { //Berührung endet
+    _down = tch;
+    if (_onTouchUp) _onTouchUp(toScreen(p));
+    if (!_drawMode && ((abs(_start.x - p.x) > _swipeX) || (abs(_start.y - p.y) > _swipeY))) {
+      if (_onTouchSwipe) _onTouchSwipe(swipeDirection(_start,p));
+    } else {
+      if ((millis()-_begin) < _clickLong) {
+        if ((millis()-_lastClick) < _dblClick) {
+          _lastClick = 0;
+          if (_onTouchDblClick) _onTouchDblClick(toScreen(p));
+        } else {
+          _lastClick=millis();
+          if (_onTouchClick) _onTouchClick(toScreen(p));
+        }
+      }
+
+    }
+  }
+
+}
+
+void TouchEvent::setResolution(int16_t xResolution, int16_t yResolution) {
+  _xResolution = xResolution;
+  _yResolution = yResolution;
+}
+
+void TouchEvent::setDrawMode(boolean drawMode) {
+  _drawMode = drawMode;
+}
+
+
+void TouchEvent::calibrate(uint16_t xMin, uint16_t yMin, uint16_t xMax, uint16_t yMax) {
+  _tsMinX = xMin;
+  _tsMinY = yMin;
+  _tsMaxX = xMax;
+  _tsMaxY = yMax;
+}
+
+void TouchEvent::setMoveTreshold(uint16_t threshold) {
+  _moveth = threshold;
+}
+
+void TouchEvent::setSwipe(uint16_t swipeX, uint16_t swipeY) {
+  _swipeX = swipeX;
+  _swipeY = swipeY;
+}
+
+void TouchEvent::setLongClick(uint16_t clickLong) {
+  _clickLong = clickLong;
+}
+
+void TouchEvent::setDblClick(uint16_t dblclick) {
+  _dblClick = dblclick;
+}
+
+void TouchEvent::registerOnTouchDown(void (*callback)(TS_Point p)) {
+  _onTouchDown = callback;
+}
+void TouchEvent::registerOnTouchUp(void (*callback)(TS_Point p)) {
+  _onTouchUp = callback;
+}
+void TouchEvent::registerOnTouchClick(void (*callback)(TS_Point p)) {
+  _onTouchClick = callback;
+}
+void TouchEvent::registerOnTouchDblClick(void (*callback)(TS_Point p)) {
+  _onTouchDblClick = callback;
+}
+void TouchEvent::registerOnTouchLong(void (*callback)(TS_Point p)) {
+  _onTouchLong = callback;
+}
+void TouchEvent::registerOnTouchDraw(void (*callback)(TS_Point p)) {
+  _onTouchDraw = callback;
+}
+void TouchEvent::registerOnTouchSwipe(void (*callback)(uint8_t direction)) {
+  _onTouchSwipe = callback;
+}
+
+boolean TouchEvent::isInArea(TS_Point p, int16_t x1, int16_t y1, int16_t x2, int16_t y2) {
+  int16_t tmp;
+  if (x1 > x2) {
+    tmp = x2; x2 = x1; x1 = tmp;
+  }
+  if (y1 > y2) {
+    tmp = y2; y2 = y1; y1 = tmp;
+  }
+  return ((p.x >= x1) && (p.x <= x2) && (p.y >= y1) && (p.y <= y2));
+}
+
+
+
+uint8_t TouchEvent::swipeDirection(TS_Point b, TS_Point e) {
+  TS_Point ptmp;
+  int bx,by,ex,ey;
+  uint8_t dir; //swipe direction 0 = left, 1 = right, 2 = up, 3 = down
+  ptmp = toScreen(b);
+  bx = ptmp.x; by = ptmp.y;
+  ptmp = toScreen(e);
+  ex = ptmp.x; ey = ptmp.y;
+  if (abs(bx - ex) > abs(by - ey)) { //bewegung in x-Richtung
+    dir = ((bx-ex) > 0)?0:1;
+  } else {//bewegung in y Richtung
+    dir = ((by-ey) > 0)?2:3;
+  }
+  return dir;
+}
+
+TS_Point TouchEvent::toScreen(TS_Point p) {
+  TS_Point p1;
+  if (abs(_xResolution) > abs(_yResolution)) {// orientation is landscape
+    if (_xResolution > 0) {
+      p1.x = map(p.x, _tsMinX, _tsMaxX, 0, _xResolution);
+    } else {
+      p1.x = map(p.x, _tsMinX, _tsMaxX, -_xResolution, 0);
+    }
+    if (_yResolution > 0) {
+      p1.y = map(p.y, _tsMinY, _tsMaxY, 0, _yResolution);
+    } else {
+      p1.y = map(p.y, _tsMinY, _tsMaxY, -_yResolution, 0);
+    }
+  } else {
+    if (_xResolution > 0) {
+      p1.x = map(p.y, _tsMinY, _tsMaxY, _xResolution, 0);
+    } else {
+      p1.x = map(p.y, _tsMinY, _tsMaxY, 0, -_xResolution);
+    }
+    if (_yResolution > 0) {
+      p1.y = map(p.x, _tsMinX, _tsMaxX, 0, _yResolution);
+    } else {
+      p1.y = map(p.x, _tsMinX, _tsMaxX, -_yResolution, 0);
+    }
+  }
+  return p1;
+}
--- src/TouchEvent.h
+++ src/TouchEvent.h
@@ -1,1 +1,154 @@
+/*
+||
+|| @file Touchevent.h
+|| @version 0.1
+|| @author Gerald Lechner
+|| @contact lechge@gmail.com
+||
+|| @description
+|| | TouchEvent extends the functionality of the XPT2046_Touchscreen
+|| | library by Paul Stoffregen
+|| | It offers an easy way to implement click, doubleclick, draw and swipe
+|| | on a Touchscreen using the XPT2046 controller
+|| #
+||
+|| @license
+|| | This library is free software; you can redistribute it and/or
+|| | modify it under the terms of the GNU Lesser General Public
+|| | License as published by the Free Software Foundation; version
+|| | 2.1 of the License.
+|| |
+|| | This library is distributed in the hope that it will be useful,
+|| | but WITHOUT ANY WARRANTY; without even the implied warranty of
+|| | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+|| | Lesser General Public License for more details.
+|| |
+|| | You should have received a copy of the GNU Lesser General Public
+|| | License along with this library; if not, write to the Free Software
+|| | Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+|| #
+||
+*/
+#ifndef TouchEvent_h
+#define TouchEvent_h
+#include <Arduino.h>
+#include <XPT2046_Touchscreen.h>
 
+#define MOVETRESHOLD 10 //define the minimal movement to detect a move
+#define SWIPEX 500 //define a minimum X-movement to detect an x swipe
+#define SWIPEY 700 //define a minimum Y-movement to detect an y swipe
+#define CLICKLONG 1000 //define a minimum time to detect a long click
+#define DOUBLECLICK 500 //define a minimum time between two clicks to detect a doubleclick
+#define SCREENX 240 //screen resolution in pixels
+#define SCREENY 320 //screen resolution in pixels
+
+// for Touchscreen
+// Parameter für Touchscreen
+#define MINPRESSURE 10 //pressure to detect touch
+#define TS_MINX 230 //minimal x return value
+#define TS_MINY 350 //minimal y return value
+#define TS_MAXX 3700  //maximal x return value
+#define TS_MAXY 3900 //maximal y return value
+
+
+
+//typedef std::function<void(TS_Point point)> TE_TouchEvent;
+//typedef std::function<void(uint8_t direction)> TE_SwipeEvent;
+
+class TouchEvent {
+public:
+  //initializer we get a pointer to the XPT2046 driver
+  TouchEvent(XPT2046_Touchscreen& touch);
+  //this is the main function which should be called in the main loop
+  //positions will be checked and under certain conditions events will be triggered
+  void pollTouchScreen();
+  //the number of pixels for the used TFT display default is x=240 y=320
+  //if coordinates are from right to left or from bottom to top, use negative numbers
+  void setResolution(int16_t xResolution, int16_t yResolution);
+  //Switches draw mode on or off
+  //move events will be triggered only if in draw mode
+  //swipe bevents only if not in draw mode
+  void setDrawMode(boolean drawMode);
+  //values to define the top left and the bottom right values
+  //returned from the touch screen. default values are for
+  //for a 2.4 inch TFT display with 320 x 240 pixels
+  void calibrate(uint16_t xMin, uint16_t yMin, uint16_t xMax, uint16_t yMax);
+  //define the minimal difference to the last point to detect a move
+  //default = 10
+  void setMoveTreshold(uint16_t threshold);
+  //define the minimal distance between touch start and touch end to
+  //detect a swipe event default = 500 and 700
+  void setSwipe(uint16_t swipeX, uint16_t swipeY);
+  //define the minimal time in ms to interpret a click as a long click
+  //default = 1000 ms
+  void setLongClick(uint16_t clickLong);
+  //define the minimal time between to clicks to detect a double click
+  //default = 500 ms
+  void setDblClick(uint16_t dblclick);
+  //register a callback function for touch start event
+  //void onEvent(TS_Point position)
+  void registerOnTouchDown(void (*callback)(TS_Point p));
+  //register a callback function for touch en event
+  //void onEvent(TS_Point position)
+  void registerOnTouchUp(void (*callback)(TS_Point p));
+  //register a callback function for touch click event
+  //void onEvent(TS_Point position)
+  void registerOnTouchClick(void (*callback)(TS_Point p));
+  //register a callback function for touch double click event
+  //void onEvent(TS_Point position)
+  void registerOnTouchDblClick(void (*callback)(TS_Point p));
+  //register a callback function for touch long click event
+  //void onEvent(TS_Point position)
+  void registerOnTouchLong(void (*callback)(TS_Point p));
+  //register a callback function for touch draw event
+  //void onEvent(TS_Point position)
+  void registerOnTouchDraw(void (*callback)(TS_Point p));
+  //register a callback function for touch swipe event
+  //direction 0 = right to left 1 = left to right
+  //2 = bottom to top 3 = top to bottom
+  //void onEvent(uint8_t direction)
+  void registerOnTouchSwipe(void (*callback)(uint8_t direction));
+  //function to detect if a position is inside a rectangle
+  //this is useful to detect if a click hit a button
+  boolean isInArea(TS_Point p, int16_t x1, int16_t y1, int16_t x2, int16_t y2);
+
+
+private:
+  XPT2046_Touchscreen& _touch;
+  TS_Point _last; //last move position
+  TS_Point _start; //position at start touch
+  boolean _down; //last touch status
+  uint32_t _begin; //start of last touched
+  uint32_t _lastClick; //time of last click
+  boolean _long; //a long click event was already triggered
+  boolean _drawMode; //draw mode means move events but no swipe events
+  //setup
+  int16_t _xResolution = SCREENX; //screen pixles minus to flip x
+  int16_t _yResolution = SCREENY; //screen pixles minus to flip y
+  uint16_t _moveth = MOVETRESHOLD; //define the minimal movement to detect a move
+  uint16_t _swipeX = SWIPEX; //define a minimum X-movement to detect an x swipe
+  uint16_t _swipeY = SWIPEY; //define a minimum Y-movement to detect an y swipe
+  uint16_t _clickLong = CLICKLONG; //define a minimum time to detect a long click
+  uint16_t _dblClick = DOUBLECLICK; //define a minimum time between two clicks to detect a doubleclick
+
+  uint16_t _minPress = MINPRESSURE; //pressure to detect touch
+  uint16_t _tsMinX = TS_MINX; //minimal x return value
+  uint16_t _tsMinY = TS_MINY; //minimal y return value
+  uint16_t _tsMaxX = TS_MAXX;  //maximal x return value
+  uint16_t _tsMaxY = TS_MAXY; //maximal y return value
+
+  void(*_onTouchDown)(TS_Point p) = NULL;
+  void(*_onTouchUp)(TS_Point p) = NULL;
+  void(*_onTouchLong)(TS_Point p) = NULL;
+  void(*_onTouchDraw)(TS_Point p) = NULL;
+  void(*_onTouchClick)(TS_Point p) = NULL;
+  void(*_onTouchDblClick)(TS_Point p) = NULL;
+  void(*_onTouchSwipe)(uint8_t direction) = NULL;
+
+  //convert raw position into screen coordinates
+  TS_Point toScreen(TS_Point p);
+  //calculate swipe direction from start and end point
+  uint8_t swipeDirection(TS_Point b, TS_Point e);
+
+};
+#endif
